0bd38c4e2ab029da46238b86172cfcd261601a43
Oksana19761219 <o.valioniene@gmail.com>
2022-05-24 19:37:06 +0300

    django 4 templates task completed

--- autoservice_1/views.py
+++ autoservice_1/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Model, Car, Service, Order, OrderLine
 
 def index(request):
-    return HttpResponse("Labas, pasauli!")
+    service_count = Service.objects.all().count()
+    order_count = Order.objects.all().count()
+    order_finished_count = Order.objects.filter(status__exact='i').count()
+    car_count = Car.objects.all().count()
+    context = {
+        'service_count': service_count,
+        'order_count': order_count,
+        'order_finished_count': order_finished_count,
+        'car_count': car_count,
+    }
+
+
+    return render(request, 'index.html', context=context)
